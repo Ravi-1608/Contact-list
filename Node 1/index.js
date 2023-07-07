ab468f585a5ed912cd60019dd65fa9f1b39f6e97
Ravi-1608 <maneravi3332@gmail.com>
2023-07-07 08:12:54 +0530

    first commit

---
+++ Node 1/index.js
@@ -0,0 +1,80 @@
+// importing express liabrary
+const express = require('express');
+// importing path
+const path = require('path')
+
+// declaring a port 
+
+const port = 10000;
+
+// creating a express app by calling express
+const app = express();
+const Contact_list =[
+    {
+        name:"ravi",
+        phone:9874563210
+    },
+    {
+        name:"mane",
+        phone:123456987
+    }
+]
+
+
+
+// setting up template engine
+app.set('view engine','ejs');
+app.set('views',path.join(__dirname,'Views'));
+
+
+// setting up middleware
+app.use(express.urlencoded());
+app.use(express.static('assets'));
+
+
+
+// creating route
+app.get('/delete-contact/:phone',function(req,res){
+    let phone = req.params.phone;
+    let contact_index = Contact_list.findIndex(contact=>contact.phone==phone); 
+    if (contact_index != -1){
+        Contact_list.splice(contact_index,1);
+    }
+    return res.redirect('/')
+    
+});
+
+
+app.get('/', function(req,res){
+
+
+    return res.render('home',{
+        title:"ContactList",
+        contactlist:Contact_list
+
+    });
+
+    // res.send(home)        // sending data as a response.
+});
+
+app.post('/create-contact',function(req,res){
+    Contact_list.push(req.body);
+    return  res.redirect('/') ;
+});
+
+
+
+
+
+
+
+
+
+app.listen(port,function(err){
+    if(err){
+        console.log("error while creating server",err)
+        return
+    }
+    console.log("server created successfully on port :",port)
+
+});
